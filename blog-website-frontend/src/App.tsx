27914ffea8990adaacc82bc71ceff8e4b7567364
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-04-06 21:26:28 +0600

    project initiated

---
+++ blog-website-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div>
+            <h2 className="text-center text-blue-500 font-bold underline">
+                Hello World
+            </h2>
+        </div>
+    )
+}
+
+export default App
